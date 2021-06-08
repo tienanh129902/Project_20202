39ecda35c622994c4c025d04454fc0381209a7d1
= <=>
2021-06-08 21:49:26 +0700

    add file

--- project/Firmware/Core/Inc/GPSConfig.h
+++
@@ -1,7 +0,0 @@
-#ifndef _GPSCONFIG_H_
-#define _GPSCONFIG_H_
-
-#define	_GPS_USART					huart1
-#define	_GPS_DEBUG					0
-
-#endif
--- project/Firmware/MDK-ARM/project/startup_stm32l476xx.d
+++
@@ -1,1 +0,0 @@
-project\startup_stm32l476xx.o: startup_stm32l476xx.s
